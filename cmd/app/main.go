c13d832aa2b8ee0b37449e3ab425f8ebce8e22e2
burhon94 <tohirov1994@gmail.com>
2021-04-14 14:35:04 +0500

    init commit

---
+++ cmd/app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	var (
+		addr = "0.0.0.0:8080"
+		prefix = "/test/api"
+	)
+
+	router := mux.NewRouter()
+	router.HandleFunc(prefix, indexHandle).Methods("GET")
+	router.HandleFunc(prefix+"/ping", pingHandle).Methods("GET")
+
+	server := http.Server{Addr: addr, Handler: router}
+
+	panic(server.ListenAndServe())
+}
+
+func indexHandle(w http.ResponseWriter, r *http.Request)  {
+	w.WriteHeader(203)
+	w.Write([]byte("index page"))
+}
+
+func pingHandle(w http.ResponseWriter, r *http.Request)  {
+	w.WriteHeader(200)
+	w.Write([]byte("Pong!"))
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/burhon94/testCI-CD-to-AWS
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0
